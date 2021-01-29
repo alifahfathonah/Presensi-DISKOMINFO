01415fc985624f238da3b36e3607b66ab47f8d90
Izmir Ta'is Khasna Lugazi <izmirtais28@gmail.com>
2021-01-29 14:36:46 +0700

    Initial commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,7 @@
+<div class="jumbotron bg-primary text-white">
+    <h4 class="my-0">Selamat Datang di Aplikasi Presensi Online</h4>
+    <hr class="my-4">
+    <p class="lead">Aplikasi Presensi Online Berbasis Website</p>
+    <h2 class="my-0">Dinas Komunikasi dan Informatika Kota Batu</h2>
+    <hr class="my-4">
+</div>
